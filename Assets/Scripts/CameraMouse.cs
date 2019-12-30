09470915f9e542cfe7c2a37565d199eb0e4ace6e
David <david.j.steiger@web.de>
2019-12-30 17:31:03 +0100

    Südflügel done. Player installed.
    Südflügel ist komplett. Eine First-Person Kamera inklusive Steuerung wurde installiert.

---
+++ Assets/Scripts/CameraMouse.cs
@@ -0,0 +1,38 @@
+﻿//Scripts and Technique found on Youtube https://www.youtube.com/watch?v=blO039OzUZc
+//By Holistic3d, who is doing a great job explaining these functions
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class CameraMouse : MonoBehaviour
+{
+    Vector2 mouseLook;
+    Vector2 smoothV;
+    public float sensitivity = 5.0f;
+    public float smoothing = 2.0f;
+
+    GameObject player;
+
+    void Start()
+    {
+        player = this.transform.parent.gameObject;
+    }
+
+    void Update()
+    {
+        //Getting coordinates from Cursor/Mouse-Position
+        var md = new Vector2(Input.GetAxisRaw("Mouse X"), Input.GetAxisRaw("Mouse Y"));
+        smoothV.x = Mathf.Lerp(smoothV.x, md.x, 1f / smoothing);
+        smoothV.y = Mathf.Lerp(smoothV.y, md.y, 1f / smoothing);
+        mouseLook += smoothV;
+
+        //Locking the view angle from -80 to 80 degree on the y-axis
+        mouseLook.y = Mathf.Clamp(mouseLook.y, -80f, 80f);
+
+        transform.localRotation = Quaternion.AngleAxis(-mouseLook.y, Vector3.right);
+        player.transform.localRotation = Quaternion.AngleAxis(mouseLook.x, player.transform.up);
+
+    }
+}
---
+++ Assets/Scripts/CharacterControl.cs
@@ -0,0 +1,31 @@
+﻿//Technique found on Youtube https://www.youtube.com/watch?v=blO039OzUZc
+//By Holistic3d
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterControl : MonoBehaviour
+{
+
+    public float characterSpeed = 10.0F;
+
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    void Update()
+    {
+        float translation   = Input.GetAxis("Vertical") * characterSpeed;
+        float straffe       = Input.GetAxis("Horizontal") * characterSpeed;
+        translation     *= Time.deltaTime;
+        straffe         *= Time.deltaTime;
+
+        transform.Translate(straffe, 0, translation);
+
+        if (Input.GetKeyDown("escape"))
+            Cursor.lockState = CursorLockMode.None;
+
+    }
+}
